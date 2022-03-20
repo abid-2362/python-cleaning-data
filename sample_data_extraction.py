5cd0170c23ae5f18050ef24c17eab0213055de9f
Abid Ali <abdsoftfsd@gmail.com>
2022-03-20 20:09:11 +0500

    Generates a 10 rows sample data for working

--- sample_data_extraction.py
+++ sample_data_extraction.py
@@ -1,6 +1,4 @@
 import pandas as pd
 
-# artwork data has been shifted in -- extras folder
-# This program will work only if the artwork_data.csv file is also included in the ./data folder
-data = pd.read_csv("./data/artwork_data.csv", nrows=500)
+data = pd.read_csv("./data/artwork_data.csv", nrows=10)
 data.to_csv("./data/artwork_sample.csv", index=False)
